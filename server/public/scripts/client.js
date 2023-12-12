11067548b1e6aba1050574e135f619e983515a8d
Daniel Holt <holt.daniel.b@gmail.com>
2023-12-12 16:38:51 -0500

    basic table and button added

---
+++ server/public/scripts/client.js
@@ -0,0 +1,8 @@
+function handleSubmitClick (event) {
+    let firstName = document.querySelector('#firstName');
+    let lastName = document.querySelector('#lastName');
+    let id = document.querySelector('#id');
+    let title = document.querySelector('#title');
+    let annualSalary = document.querySelector('#annualSalary');
+    
+    }
