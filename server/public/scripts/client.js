615702e317e3cf47f4cf3dc0d556751522e4db0a
Daniel Holt <holt.daniel.b@gmail.com>
2023-12-12 18:16:51 -0500

    having issues with relative paths not working, for now I have used full path to write the rest of the code

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,26 @@
-function handleSubmitClick (event) {
-    let firstName = document.querySelector('#firstName');
-    let lastName = document.querySelector('#lastName');
-    let id = document.querySelector('#id');
-    let title = document.querySelector('#title');
-    let annualSalary = document.querySelector('#annualSalary');
-    
+
+function addEmployee(event) {
+    event.preventDefault();
+    console.log('submitform');
+    let firstNameVal = document.querySelector('#firstNameInput').value;
+    let lastNameVal = document.querySelector('#lastNameInput').value;
+    let idVal = document.querySelector('#idInput').value;
+    let titleVal = document.querySelector('#titleInput').value;
+    let annualSalaryVal = document.querySelector('#annualSalaryInput').value;
+    let employeeTable = document.querySelector('#employeeData');
+    employeeTable.innerHTML += `
+        <tr>
+            <td>${firstNameVal}</td>
+            <td>${lastNameVal}</td>
+            <td>#${idVal}</td>
+            <td>${titleVal}</td>
+            <td>$${annualSalaryVal}</td>
+            <td><button id='delete' onclick='handleDeleteClick(event)'>DELETE</button></td>
+        </tr>
+    `
+    }
+
+    function handleDeleteClick(event) {
+        event.preventDefault();
+        console.log('delete');
     }
